f1b09357870f8b9cdd76dbfd2727b04ff5e4cd90
Romuald Makovskij <romualdasit@gmail.com>
2022-07-17 19:19:14 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Aunt cup page
+
+## install dependecies
+```
+npm i
+```
+
+## launch server
+```
+npm run server
+```
+
+## launch client 
+```
+npm run start
+```
---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+const App = () => {
+  return (
+    <div>Aplikacija
+      <Button variant="contained">Contained</Button>
+    </div >
+  );
+}
+
+export default App;
